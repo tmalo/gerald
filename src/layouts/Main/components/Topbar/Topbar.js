411717fc57f7904765e6b15c621eeb1ceed9176c
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2020-06-23 23:21:27 +0200

    logout

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -7,6 +7,7 @@ import { AppBar, Toolbar, Badge, Hidden, IconButton } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import NotificationsIcon from "@material-ui/icons/NotificationsOutlined";
 import InputIcon from "@material-ui/icons/Input";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,6 +23,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Topbar = (props) => {
   const { className, onSidebarOpen, ...rest } = props;
+  var history = useHistory();
 
   const classes = useStyles();
 
@@ -40,7 +42,14 @@ const Topbar = (props) => {
               <NotificationsIcon />
             </Badge>
           </IconButton>
-          <IconButton className={classes.signOutButton} color="inherit">
+          <IconButton
+            className={classes.signOutButton}
+            color="inherit"
+            onClick={() => {
+              localStorage.removeItem("token");
+              history.push("/sign-in");
+            }}
+          >
             <InputIcon />
           </IconButton>
         </Hidden>
